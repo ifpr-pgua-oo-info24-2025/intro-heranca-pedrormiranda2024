a43d7f203769fe6ccf1ef9d03f5ad28dd73b16df
Ana Flávia <anaflavia.aurichio.niz@gmail.com>
2025-05-23 12:16:19 -0300

    incompleto

---
+++ heranca/src/main/java/com/rodrigo/pedro/Main.java
@@ -0,0 +1,15 @@
+package com.rodrigo.pedro;
+import main.java.com.rodrigo.pedro.models.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Moto moto = new Moto("Kawasaki", "Ninja", 2024, "Reto");
+    Carro carro = new Carro("Fiat", "Uno", 1980, 2);
+
+    moto.setAno(2022);
+    carro.setModelo("Mobi");
+
+    System.out.println(moto.exibirDetalhes());
+    System.out.println(carro.exibirDetalhes());
+  }
+}
---
+++ heranca/src/main/java/com/rodrigo/pedro/models/Moto.java
@@ -0,0 +1,23 @@
+package main.java.com.rodrigo.pedro.models;
+
+public class Moto extends Veiculo{
+    private String tipoGuidao;
+
+    public Moto(String marca, String modelo, int ano, String tipoGuidao) {
+        super(marca, modelo, ano);
+        this.tipoGuidao = tipoGuidao;
+    }
+
+    public String getTipoGuidao() {
+        return tipoGuidao;
+    }
+
+    public void setTipoGuidao(String tipoGuidao) {
+        this.tipoGuidao = tipoGuidao;
+    }
+    public String exibirDetalhes() {
+        Veiculo.exibirDetalhes();
+        return "Moto [tipoGuidao=" + tipoGuidao + "]";
+    }
+
+}
